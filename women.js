c0df8b9dfde098041ceffe7c3f3d7ca16d767099
arsh459 <arshgoyal459@gmail.com>
2023-01-08 00:28:50 +0530

    Women cart Corrected

--- women.js
+++ women.js
@@ -2,7 +2,7 @@ var data=[
     {
         Brand:"Bewakoof",
         Desc: "Women's Red & White Classic Mickey Striped Oversized T-shirt",
-        image_url:"https://images.bewakoof.com/t1080/women-classic-mickey-f-b-printed-t-shirt-563353-1672312058-1.jpg",
+        img:"https://images.bewakoof.com/t1080/women-classic-mickey-f-b-printed-t-shirt-563353-1672312058-1.jpg",
         price:779,
         cprice:1699,
         tprice:539,
@@ -12,7 +12,7 @@ var data=[
     {
         Brand:"Bewakoof",
         Desc: "Women's Black Pop Minnie Graphic Printed Oversized T-shirt",
-        image_url:"https://images.bewakoof.com/t1080/women-pop-minnie-printed-t-shirt-563362-1672134630-1.jpg",
+        img:"https://images.bewakoof.com/t1080/women-pop-minnie-printed-t-shirt-563362-1672134630-1.jpg",
         price:599,
         cprice:1299,
         tprice:509,
@@ -22,7 +22,7 @@ var data=[
     {
         Brand:"Bewakoof",
         Desc: "Men's Blue Typography Oversized Fit T-shirt",
-        image_url:"https://images.bewakoof.com/t640/women-s-black-abstract-printed-relaxed-fit-velvet-jacket-559402-1670586990-1.jpg",
+        img:"https://images.bewakoof.com/t640/women-s-black-abstract-printed-relaxed-fit-velvet-jacket-559402-1670586990-1.jpg",
         price:1439,
         cprice:1799,
         tprice:339,
@@ -33,7 +33,7 @@ var data=[
     {
         Brand:"Bewakoof",
         Desc: "Women's Black Get Going Snoopy Graphic Printed Boyfriend T-shirt",
-        image_url:"https://images.bewakoof.com/t1080/get-going-snoopy-boyfriend-t-shirt-516609-1657202693-1.jpg",
+        img:"https://images.bewakoof.com/t1080/get-going-snoopy-boyfriend-t-shirt-516609-1657202693-1.jpg",
         price:399,
         cprice:999,
         tprice:599,
@@ -43,7 +43,7 @@ var data=[
     {
         Brand:"Aks",
         Desc:"Women's Weekend Mood Tom Slim Fit T-shirt",
-        image_url:"https://images.bewakoof.com/t1080/weekend-mood-tom-tjl-half-sleeve-t-shirt-354953-1655748478-1.jpg",
+        img:"https://images.bewakoof.com/t1080/weekend-mood-tom-tjl-half-sleeve-t-shirt-354953-1655748478-1.jpg",
         price:299,
         cprice:1099,
         tprice:599,
@@ -53,7 +53,7 @@ var data=[
     {
         Brand:"Aks",
         Desc:"Women's Blue The View Graphic Printed Boyfriend T-shirt",
-        image_url:"https://images.bewakoof.com/t1080/women-s-blue-the-view-graphic-printed-boyfriend-t-shirt-565899-1672411151-1.jpg",
+        img:"https://images.bewakoof.com/t1080/women-s-blue-the-view-graphic-printed-boyfriend-t-shirt-565899-1672411151-1.jpg",
         price:349,
         cprice:999,
         tprice:399,
@@ -64,7 +64,7 @@ var data=[
     {
         Brand:"Aks",
         Desc:"Black Basic Shorts",
-        image_url:"https://images.bewakoof.com/t1080/black-basic-shorts-330809-1655748409-1.jpg",
+        img:"https://images.bewakoof.com/t1080/black-basic-shorts-330809-1655748409-1.jpg",
         price:449,
         cprice:999,
         tprice:599,
@@ -74,7 +74,7 @@ var data=[
     {
         Brand:"Aks",
         Desc:"Women's Black & Sage Reversible Super Loose Puffer Jacket",
-        image_url:"https://images.bewakoof.com/t1080/women-s-black-puffer-reversible-jacket-499009-1670255534-1.jpg",
+        img:"https://images.bewakoof.com/t1080/women-s-black-puffer-reversible-jacket-499009-1670255534-1.jpg",
         price:1999,
         cprice:4499,
         tprice:999,
@@ -84,7 +84,7 @@ var data=[
     {
         Brand:"Kotty",
         Desc:"Women's Blue Bomber Jacket",
-        image_url:"https://images.bewakoof.com/t1080/women-s-deep-teal-solid-bomber-jacket-498991-1664287377-1.jpg",
+        img:"https://images.bewakoof.com/t1080/women-s-deep-teal-solid-bomber-jacket-498991-1664287377-1.jpg",
         price:1349,
         cprice:3499,
         tprice:799,
@@ -94,7 +94,7 @@ var data=[
     {
         Brand:"Kotty",
         Desc:"Women's Deep Teal Relaxed Fit Puffer Jacket",
-        image_url:"https://images.bewakoof.com/t1080/women-s-deep-teal-solid-puffer-jackets-499027-1670307277-1.jpg",
+        img:"https://images.bewakoof.com/t1080/women-s-deep-teal-solid-puffer-jackets-499027-1670307277-1.jpg",
         price:1499,
         cprice:3499,
         tprice:799,
@@ -104,7 +104,7 @@ var data=[
     {
         Brand:"Kotty",
         Desc:"Women's Black Biker Jacket",
-        image_url:"https://images.bewakoof.com/t1080/women-s-black-biker-jacket-559198-1670584068-1.jpg",
+        img:"https://images.bewakoof.com/t1080/women-s-black-biker-jacket-559198-1670584068-1.jpg",
         price:899,
         cprice:1799,
         tprice:499,
@@ -114,7 +114,7 @@ var data=[
     {
         Brand:"Kotty",
         Desc:"Women's Orchid Petal Cut Out Dress",
-        image_url:"https://images.bewakoof.com/t1080/orchid-petal-rib-cut-out-solid-dress-464327-1655731714-1.jpg",
+        img:"https://images.bewakoof.com/t1080/orchid-petal-rib-cut-out-solid-dress-464327-1655731714-1.jpg",
         price:359,
         cprice:1499,
         tprice:349,
@@ -124,7 +124,7 @@ var data=[
     {
         Brand:"Rigo",
         Desc:"Women's Blue All Over Pineapple Printed Oversized Dress",
-        image_url:"https://images.bewakoof.com/t1080/pineapple-oversized-dress-516725-1669035593-1.jpg",
+        img:"https://images.bewakoof.com/t1080/pineapple-oversized-dress-516725-1669035593-1.jpg",
         price:899,
         cprice:1899,
         tprice:219,
@@ -134,7 +134,7 @@ var data=[
     {
         Brand:"Rigo",
         Desc:"Women's Black All Over Printed Oversized Dress",
-        image_url:"https://images.bewakoof.com/t1080/women-s-black-all-over-printed-oversized-dress-467420-1656153117-1.jpg",
+        img:"https://images.bewakoof.com/t1080/women-s-black-all-over-printed-oversized-dress-467420-1656153117-1.jpg",
         price:599,
         cprice:1899,
         tprice:219,
@@ -144,7 +144,7 @@ var data=[
     {
         Brand:"Rigo",
         Desc:"Women's Blue Printed 3/4th Sleeve Ethnic Dress",
-        image_url:"https://images.bewakoof.com/t1080/women-s-3-4th-printed-ethnic-dress-403814-1663920864-1.jpg",
+        img:"https://images.bewakoof.com/t1080/women-s-3-4th-printed-ethnic-dress-403814-1663920864-1.jpg",
         price:649,
         cprice:2499,
         tprice:579,
@@ -154,7 +154,7 @@ var data=[
     {
         Brand:"Rigo",
         Desc:"Ceylon Yellow Elbow Sleeve Pocket Dress",
-        image_url:"https://images.bewakoof.com/t1080/ceylon-yellow-elbow-sleeve-pocket-dress-417960-1656140124-1.jpg",
+        img:"https://images.bewakoof.com/t1080/ceylon-yellow-elbow-sleeve-pocket-dress-417960-1656140124-1.jpg",
         price:429,
         cprice:1899,
         tprice:479,
@@ -164,7 +164,7 @@ var data=[
     {
         Brand:"Rigo",
         Desc:"Women's Green AOP Oversized T-shirt Dress",
-        image_url:"https://images.bewakoof.com/t1080/women-s-aop-boyfriend-fit-t-shirt-dress-507970-1670307480-1.jpg",
+        img:"https://images.bewakoof.com/t1080/women-s-aop-boyfriend-fit-t-shirt-dress-507970-1670307480-1.jpg",
         price:799,
         cprice:1699,
         tprice:679,
@@ -174,7 +174,7 @@ var data=[
     {
         Brand:"Freakins",
         Desc:"Women's Very Peri Slim Fit Slip Dress",
-        image_url:"https://images.bewakoof.com/t1080/women-s-very-peri-slip-dress-521199-1659110753-1.jpg",
+        img:"https://images.bewakoof.com/t1080/women-s-very-peri-slip-dress-521199-1659110753-1.jpg",
         price:503,
         cprice:1599,
         tprice:509,
@@ -184,7 +184,7 @@ var data=[
     {
         Brand:"Freakins",
         Desc:"Women's Maroon Floral Printed Dress",
-        image_url:"https://images.bewakoof.com/t1080/women-s-maroon-floral-printed-dress-547453-1665145347-1.JPG",
+        img:"https://images.bewakoof.com/t1080/women-s-maroon-floral-printed-dress-547453-1665145347-1.JPG",
         price:765,
         cprice:1699,
         tprice:309,
@@ -194,7 +194,7 @@ var data=[
     {
         Brand:"Door",
         Desc:"Women's Blue Typography Oversized T-shirt",
-        image_url:"https://images.bewakoof.com/t1080/women-s-blue-typography-oversized-t-shirt-539714-1662108263-1.JPG",
+        img:"https://images.bewakoof.com/t1080/women-s-blue-typography-oversized-t-shirt-539714-1662108263-1.JPG",
         price:765,
         cprice:1699,
         tprice:309,
@@ -204,7 +204,7 @@ var data=[
     {
         Brand:"Door ",
         Desc:"Women's Black You Typography Oversized T-shirt",
-        image_url:"https://images.bewakoof.com/t1080/women-s-black-you-typography-oversized-t-shirt-539647-1662107781-1.JPG",
+        img:"https://images.bewakoof.com/t1080/women-s-black-you-typography-oversized-t-shirt-539647-1662107781-1.JPG",
         price:599,
         cprice:1499,
         tprice:569,
@@ -214,7 +214,7 @@ var data=[
     {
         Brand:"Door ",
         Desc:"Women's Black Color Block Oversized T-shirt",
-        image_url:"https://images.bewakoof.com/t1080/women-s-black-color-block-oversized-t-shirt-539655-1662107959-1.jpg",
+        img:"https://images.bewakoof.com/t1080/women-s-black-color-block-oversized-t-shirt-539655-1662107959-1.jpg",
         price:499,
         cprice:1199,
         tprice:469,
@@ -224,7 +224,7 @@ var data=[
     {
         Brand:"Door ",
         Desc:"Women's White Typography Oversized T-shirt",
-        image_url:"https://images.bewakoof.com/t1080/women-s-white-typography-oversized-t-shirt-539649-1662107670-1.JPG",
+        img:"https://images.bewakoof.com/t1080/women-s-white-typography-oversized-t-shirt-539649-1662107670-1.JPG",
         price:599,
         cprice:1499,
         tprice:369,
@@ -248,7 +248,7 @@ function display(data){
         var div=document.createElement("div");
         div.setAttribute('class',"child")
         var img=document.createElement("img");
-        img.setAttribute("src",e.image_url);
+        img.setAttribute("src",e.img);
         img.addEventListener("click",function(){
             addToCart(e,i);
         });
@@ -355,6 +355,20 @@ function sortByPrice(){
     }
 }
 
+var cart=JSON.parse(localStorage.getItem("cart")) || [];
 function addToCart(e,i){
-    alert("added to cart")
+    var result=cart.filter(function(elem,index){
+        if(e.identity==elem.identity){
+            alert("Item Aleady in Cart");
+            return true;
+        }
+    })
+    if(result.length==0){
+        e.quantity=1;
+        e.fcprice=e.quantity*e.cprice;
+        e.ftprice=e.quantity*e.tprice;
+        e.fprice=e.quantity*e.price;
+        cart.push(e);
+        localStorage.setItem("cart",JSON.stringify(cart));
+    }
 }
