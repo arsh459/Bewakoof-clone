b39855917c622e65c993fb08ad42306666cd8ec5
pranshushuklagithub <pranshushuklaoffice@gmail.com>
2023-01-08 20:19:30 +0530

    mobile pages are updated and ready to merge

--- mobilepages.js
+++ mobilepages.js
@@ -1,3 +1,5 @@
+// Data Showing In Web Page By Creating Html Elements
+
 display(data);
 function display(list) {
     document.querySelector("#Containers").textContent = "";
@@ -9,15 +11,15 @@ function display(list) {
             });
     
             var image = document.createElement("img");
-            image.setAttribute("src", elem.img_url);
+            image.setAttribute("src", elem.img);
             image.setAttribute("alt", elem.id);
 
             title = document.createElement("p");
-            title.textContent = elem.title;
+            title.textContent = elem.Brand;
             title.setAttribute("id","brandTitle")
 
             var desc = document.createElement("p");
-            desc.textContent = elem.desc;
+            desc.textContent = elem.Desc;
             desc.setAttribute("id","description")
 
             var priceBox = document.createElement("div");
@@ -27,7 +29,7 @@ function display(list) {
             price.textContent = "₹" + elem.price;
 
             var mrp = document.createElement("p");
-            mrp.textContent = "₹" + elem.strikedOffPrice;
+            mrp.textContent = "₹" + elem.cprice;
 
             var box = document.createElement("div");
             box.setAttribute("id","box_of_rating_and_price");
@@ -53,28 +55,49 @@ function display(list) {
         }) 
         
         if(list.length === 0) {
-            document.querySelector("h1").textContent = "Sorry ! Not Available For Now";
-        }else {
+            document.querySelector("h1").textContent = "Sorry ! Currently Unavailable";
+        }else  {
             document.querySelector("h1").textContent = document.querySelector("title").textContent + "(" + list.length  +")";
         }
         // console.log(data);
     }
 
-    var cartItems = JSON.parse(localStorage.getItem("cart")) || [];
-    function addToCart(item) {
-        cartItems.push(item);
-        localStorage.setItem("cart",JSON.stringify(cartItems));
-    }
 
+
+    // Add To Cart Function 
+
+
+    var cart=JSON.parse(localStorage.getItem("cart")) || [];
+    function addToCart(e,i){
+    var result=cart.filter(function(elem,index){
+        if(e.identity==elem.identity){
+            return true;
+        }
+        else{
+            return false;
+        }
+    })
+    if(result.length==0){
+        e.quantity=1;
+        e.fcprice=e.quantity*e.cprice;
+        e.ftprice=e.quantity*e.tprice;
+        e.fprice=e.quantity*e.price;
+        cart.push(e);
+        localStorage.setItem("cart",JSON.stringify(cart));
+    }
+}
+
+// Search Function
     
     function search() {
         var searchForItem = document.getElementById("searchForItem").value;
         var relatedItem = [];
         data.filter(function(elem) {
-            if((elem.title).toLowerCase() == searchForItem.toLowerCase()) {
+            if((elem.Brand).toLowerCase() == searchForItem.toLowerCase()) {
                 relatedItem.push(elem);
             }
         })
+
         display(relatedItem);
         document.getElementById("searchForItem").value = "";
     }
